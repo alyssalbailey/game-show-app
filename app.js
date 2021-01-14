813300ced9757db4a1e03b6ba5a0d705c77db7fe
alyssalbailey <alyssabailey@me.com>
2021-01-13 21:07:04 -0500

    check letter function

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const buttonReset = document.getElementsByClassName('btn__reset')[0];
 const missed = 0;
 const letterButton = document.getElementById('keyrow');
 
+
 //Phrases to guess while in gameplay
 const phrases = [
     'hello world',
@@ -18,52 +19,53 @@ const phrases = [
 const characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 buttonReset.addEventListener('click', () => {
-        buttonReset.style.display = "none";
+        buttonReset.parentNode.style.display = "none";
     });
 
 // Return a random phrase from the phrases array
 function getRandomPhraseAsArray(arr) {
     let randomNumber = Math.floor(Math.random() * phrases.length);
-    return phrases[randomNumber];
+    return phrases[randomNumber].split('');
 }
 
 //Calling the getRandomPhrasesAsArray function to check if it's working properly
 console.log(getRandomPhraseAsArray(phrases));
 
-//Add the letters of a string to the display - done!
+//Add the letters of a string to the display
 function addPhraseToDisplay(arr) {
-    let letters = '';
+    const ul = phrase.querySelector('ul');
     for (let i = 0; i < arr.length; i++){
-        letters += `<li>${arr[i]}</li>`;
+        let li = document.createElement('li');
+        li.textContent = arr[i];
+        ul.appendChild(li);
+        if (arr[i] !== ' ') {
+            li.classList.add('letter');
+        } else {
+            li.classList.add('space');
+
+        }
     }
-    return letters;
-
 }
-
-document.querySelectorAll('phrase').innerHTML = `
-        <ul>
-            <li>${addPhraseToDisplay(characters)}</li>
-        </ul>
-    `;
-
-
 
 const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
 
-//Check if a letter is in the random phrase - INCOMPLETE
-function checkLetter(letterButton) {
-    const listItem = document.querySelectorAll('li');
-    const matchLetter = null;
-    let total = 0;
-    for (let i = 0; i < listItem.length; i++);
-    total += listItem[i];
-    // if letter is part of the phrase, then add the 'show class' 
+//Check if a letter is in the random phrase 
+function checkLetter(button) {
+    const li = document.querySelectorAll('.letter');
+    let match = null;
+    for (let i = 0; i < li.length; i++);
+        if (li[i].textContent.toLowerCase() === button ) {
+            li[i].classList.add('show');  
+            match = li[i].textContent;
+        }
+    }
+       return match;
 }
 
-
-//Event listener for 'press button' on the screen keyboard
-qwerty.addEventListener('click', () => {
+/*Event listener for 'press button' on the screen keyboard
+qwerty.addEventListener('click', e => {
     //missing statements
 
-});
+
+}); */
